810dbaa345865cec5366404b4411a99052fda053
Michael-Levick <mdlevick@gmail.com>
2019-10-21 18:11:01 -0500

    commented out app.js work

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-<<<<<<< HEAD
 import React from "react";
 import "./App.css";
-import DivLib from "./components/DevLib";
-=======
-import React, {useState} from 'react';
-
-import './App.css';
-import './styling/madlibInput.css'
-
->>>>>>> 22228ce42151fde80a14753fcb3ca0d96b20fcc2
+// import DivLib from "./components/DevLib";
 
 function App() {
-
- 
-
-  return (
-    <div className="App">
-<<<<<<< HEAD
-      <DivLib />
-=======
-       
->>>>>>> 22228ce42151fde80a14753fcb3ca0d96b20fcc2
-    </div>
-  );
+  return <div className="App">{/* <DivLib /> */}</div>;
 }
 
 export default App;
