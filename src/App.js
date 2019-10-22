9bf7ae9d232f0a1fe51083393ffe9f79ef85e536
Michael-Levick <mdlevick@gmail.com>
2019-10-22 17:33:13 -0500

    working on API for DevLib

--- src/App.js
+++ src/App.js
@@ -1,58 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Welcomepage from "./components/Welcomepage";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoute";
-import Navbar from "./components/NavBar";
+// import PrivateRoute from "./components/PrivateRoute";
+import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
-import './App.css';
-import DivLib from "./components/DevLib";
-import MadlibInput from './components/MadlibInput'
-import {WordsContext} from './hooks/WordsContext'
-import {MadLibContext} from './hooks/MadLibContext'
+import "./App.css";
+// import DevLib from "./components/DevLib";
+import MadlibInput from "./components/MadlibInput";
+// import { WordsContext } from "./hooks/WordsContext";
+import { MadLibContext } from "./hooks/MadLibContext";
 
 function App(props) {
-
   const [words, setWords] = useState({
-    noun:'',
-    verb:'',
-    adverb:'',
-    adjective:''
-  })
-
-
+    noun: "",
+    verb: "",
+    adverb: "",
+    adjective: ""
+  });
 
   return (
     <div className="App">
       <Navbar />
       <Router>
         <Switch>
-          <MadLibContext.Provider value = {{words, setWords}}>
-           <WordsContext.Provider>
-              <Route exact path="/" component={Welcomepage} /> 
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              {/* <PrivateRoute exact path="/profile" component={Profile} />  */}
-              {/* <DivLib /> */}
-              <Route exact path="/profile" component={Profile} />
-               <MadlibInput props={props}/>
-           </WordsContext.Provider>
+          <MadLibContext.Provider value={{ words, setWords }}>
+            {/* <WordsContext.Provider> */}
+            <Route exact path="/" component={Welcomepage} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            {/* <PrivateRoute exact path="/profile" component={Profile} />  */}
+            {/* <DevLib /> */}
+            <Route exact path="/profile" component={Profile} />
+            <MadlibInput props={props} />
+            {/* </WordsContext.Provider> */}
           </MadLibContext.Provider>
-    
-             
         </Switch>
       </Router>
     </div>
   );
-
-
-
-
-
-
-
 }
 
 export default App;
-
