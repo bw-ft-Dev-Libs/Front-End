2303cb72a21fe48cfa4e1c966d34937635ade5f6
Michael-Levick <mdlevick@gmail.com>
2019-10-23 18:04:24 -0500

    working on edit

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,15 +1,11 @@
-import React, { useState } from "react";
-import { Card } from "../styles/CardStyles";
+import React from "react";
+
+import "../styling/modal.css";
 
 const ProfileCard = props => {
   return (
     <div>
-      <Card>
-        <p>{props.lib.lib}</p>
-      </Card>
-
-      <button>Edit</button>
-      <button>Delete</button>
+      <p>{props.lib.lib}</p>
     </div>
   );
 };
